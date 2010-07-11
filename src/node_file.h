5342b665bfef4062e9a687d18d6a9a0d272badeb
Ryan Dahl <ry@tinyclouds.org>
2010-07-11 14:53:14 +0200

    EventSource for File I/O

--- src/node_file.h
+++ src/node_file.h
@@ -5,12 +5,41 @@
 #include <node.h>
 #include <node_events.h>
 #include <v8.h>
+#include <eio.h>
 
 namespace node {
 
 class File {
  public:
   static void Initialize(v8::Handle<v8::Object> target);
+};
+
+class EIORequest : public EventSource {
+ public:
+  static void Initialize(v8::Handle<v8::Object> target);
+
+  static EIORequest* New();
+
+  EIORequest() : EventSource() { }
+  ~EIORequest() { }
+
+  void Set(eio_req *req, v8::Handle<v8::Value> callback);
+
+  static int After(eio_req *req);
+
+  void Active() {
+    EventSource::Active();
+    ev_ref(EV_DEFAULT_UC);
+  }
+
+  void Inactive() {
+    ev_unref(EV_DEFAULT_UC);
+    req_ = NULL;
+    EventSource::Inactive();
+  }
+
+ private:
+  eio_req *req_;
 };
 
 }  // namespace node
